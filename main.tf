dfb2eef7319308c232e8a4296c2242356099cc4c
deepa3006 <deepu.palabhavi@gmail.com>
2023-01-09 15:35:42 +0100

    Updates

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "dataset" {
 module "table" {
   depends_on = [module.dataset] 
   source = "./table"
-  furniture_id = module.dataset.furniture.dataset_id
+  furniture_id = module.dataset.furniture_dataset_id
   tbl_chair = var.tbl_chair
   tbl_sofa = var.tbl_sofa
 }
