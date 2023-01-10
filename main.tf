6ad9b64b84c25501cf4cc721dd2534afa111f236
deepa3006 <deepu.palabhavi@gmail.com>
2023-01-10 10:48:01 +0100

    Updates

--- main.tf
+++ main.tf
@@ -13,15 +13,19 @@ provider "google" {
 # modules
 module "dataset" {
   source = "./dataset"
-  furniture_DS_location = var.furniture_DS_location
-  furniture = var.furniture
+  #furniture_DS_location = var.furniture_DS_location
+  #furniture = var.furniture
+  furniture_DS_location = "US"
+  furniture = "furniture"
 
 }
 
-module "table" {
-  depends_on = [module.dataset] 
-  source = "./table"
-  furniture_id = module.dataset.furniture_dataset_id
-  tbl_chair = var.tbl_chair
-  tbl_sofa = var.tbl_sofa
-}
+# module "table" {
+#   depends_on = [module.dataset] 
+#   source = "./table"
+#   furniture_id = module.dataset.furniture_dataset_id
+#   #tbl_chair = var.tbl_chair
+#   tbl_chair = "tbl_chair"
+#   #tbl_sofa = var.tbl_sofa
+#   tbl_sofa = "tbl_sofas"
+# }
--- terraform.tfvars
+++
@@ -1,6 +0,0 @@
-
-tbl_sofa="tbl_sofas"
-tbl_chair="tbl_chairs"
-
-furniture="furniture"
-furniture_DS_location="US"
